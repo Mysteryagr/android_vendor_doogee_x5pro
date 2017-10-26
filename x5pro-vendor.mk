cba1472943135c12014122600928b547220e31f0
SeriniTY320 <SeriniTY320@gmail.com>
2017-10-26 19:13:41 +0000

    Delete Prebuild APK

--- x5pro-vendor.mk
+++ x5pro-vendor.mk
@@ -1,19 +1,19 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/priv-app,system/priv-app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/priv-app,system/priv-app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/bin,system/vendor/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/vendor/bin,system/vendor/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/etc,system/vendor/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/vendor/etc,system/vendor/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/firmware,system/vendor/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/vendor/firmware,system/vendor/firmware)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/lib,system/vendor/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/vendor/lib,system/vendor/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/e4/vendor/thh,system/vendor/thh)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/x5pro/vendor/thh,system/vendor/thh)
